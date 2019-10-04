8e3ca0aa25a024f35d372b6382ebec0201100a68
Russell Dermody <russ.dermody@gmail.com>
2019-10-03 23:25:57 -0700

    Book Schema Built

--- models/book.js
+++ models/book.js
@@ -2,4 +2,30 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const bookSchema = new Schema({});
+const bookSchema = new Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  authors: [
+    {
+      type: String,
+      required: true
+    }
+  ],
+  description: {
+    type: String,
+    required: true
+  },
+  link: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: String,
+    required: true
+  }
+});
+
+const Book = mongoose.model("Book", bookschema);
+module.exports = Book;
