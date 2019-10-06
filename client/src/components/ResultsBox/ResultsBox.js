10f9b6f49a1e3645cc33e38a4e53d5b173ae0387
Russ <russ.dermody@gmail.com>
2019-10-05 21:47:04 -0700

    Search works, moderately styled

--- client/src/components/ResultsBox/ResultsBox.js
+++ client/src/components/ResultsBox/ResultsBox.js
@@ -1,24 +1,37 @@
 import React from "react";
 import "./ResultsBox.css";
 
+// *Componennt Import
+import CardColumns from "react-bootstrap/CardColumns";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+
 const ResultsBox = props => {
   // trying CS6..
   return props.books.length === 0 ? (
-    <div>No Search Results</div>
+    <div className="resultsBox">No Results Found.</div>
   ) : (
-    <div>
-      Results
-      {props.books.map(book => {
-        return (
-          <>
-            <p>{book.title}</p>
-            <p>{book.author}</p>
-            <p>{book.description}</p>
-          </>
-        );
-      })}
+    <div className="resultsBox">
+      <CardColumns>
+        {props.books.map(book => {
+          return (
+            <Card className="book">
+              <Card.Img variant="top" src={book.image} />
+              <Card.Body>
+                <Card.Title>
+                  <span className="title">{book.title}</span>
+                </Card.Title>
+                <Card.Text>{book.description}</Card.Text>
+              </Card.Body>
+            </Card>
+          );
+        })}
+      </CardColumns>
     </div>
   );
 };
+// <p>{book.title}</p>
+// <p>{book.author}</p>
+// <p>{book.description}</p>
 
 export default ResultsBox;
